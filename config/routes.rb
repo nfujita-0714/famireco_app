b06d3884a42aebaf2fdd8355211a45eacfc21402
nfujita-0714 <nfujita0714@gmail.com>
2021-10-10 09:58:24 +0000

    フォーム修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :pictures
   root 'pictures#index'
+  resources :pictures
+  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
