67164f96d16d86befea8aab2a56131130032d42e
nfujita-0714 <nfujita0714@gmail.com>
2021-10-11 01:47:19 +0000

    お気に入り機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_000916) do
+ActiveRecord::Schema.define(version: 2021_10_11_012216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_favorites_on_picture_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "labelings", force: :cascade do |t|
     t.bigint "label_id"
@@ -54,6 +63,8 @@ ActiveRecord::Schema.define(version: 2021_10_11_000916) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "pictures"
+  add_foreign_key "favorites", "users"
   add_foreign_key "labelings", "labels"
   add_foreign_key "labelings", "pictures"
   add_foreign_key "pictures", "users"
