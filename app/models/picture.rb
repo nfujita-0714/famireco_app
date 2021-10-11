63d81d15564d7a48381b1adef2f74e756a4fd28f
nfujita-0714 <nfujita0714@gmail.com>
2021-10-11 00:45:57 +0000

    アソシエーション

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  has_many :labelings, dependent: :destroy
+  has_many :labels, through: :labelings
 
   mount_uploader :image, ImageUploader
 end
