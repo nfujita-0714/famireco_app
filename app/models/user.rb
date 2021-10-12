8ced633c9101f398810d2af3356d2cbc524fad70
nfujita-0714 <nfujita0714@gmail.com>
2021-10-12 07:07:24 +0000

    ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :fav_pictures, through: :favorites, source: :picture
 
+  def self.guest
+    find_or_create_by!(name: 'ゲスト', self_introduction: 'ゲストです！お試し中です', email: 'guest@example.com') do |user|
+      user.password = "password"
+    end
+  end
+  
+  def self.admin_guest
+    find_or_create_by!(name: '管理者ゲスト', self_introduction: '管理者ゲストです。お試し中です', email: 'admin_guest@example.com', admin: true) do |user|
+      user.password = SecureRandom.alphanumeric()
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,16 @@ Rails.application.routes.draw do
   end
   
   resources :favorites, only: [:create, :destroy, :index]
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   resources :users, only: :show
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/guest_admin_sign_in', to: 'users/sessions#guest_admin_sign_in'
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
