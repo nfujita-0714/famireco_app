45d5ae814d7ac61136d2a008d662babbb9db7f42
nfujita-0714 <nfujita0714@gmail.com>
2021-10-12 03:39:31 +0000

    rails_admin追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'tops#index'
   resources :pictures
   resources :favorites, only: [:create, :destroy, :index]
